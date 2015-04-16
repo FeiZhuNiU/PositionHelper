f71847dffbf78e8670f043574a4c94dff335b41d
Eric YU <lin.yu@oracle.com>
2015-04-16 14:28:21 +0800

    add mapView , and show where the location actually is

--- app/src/main/java/com/yu/eric/positionhelper/MainActivity.java
+++ app/src/main/java/com/yu/eric/positionhelper/MainActivity.java
@@ -1,11 +1,26 @@
 package com.yu.eric.positionhelper;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.TextView;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 
 
 public class MainActivity extends Activity implements LocationWatcher{
@@ -13,73 +28,51 @@ public class MainActivity extends Activity implements LocationWatcher{
     private TextView latitudeText;
     private TextView longitudeText;
     private TextView updateTime;
+    private MapView mapView;
 
     private static Location location = null;
 
     private static final String TAG ="MainActivity";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        latitudeText = (TextView) findViewById(R.id.latitudeText);
-        longitudeText = (TextView) findViewById(R.id.longitudeText);
-        updateTime = (TextView) findViewById(R.id.time_updated);
+
+
+        latitudeText    = (TextView) findViewById(R.id.latitudeText);
+        longitudeText   = (TextView) findViewById(R.id.longitudeText);
+        updateTime      = (TextView) findViewById(R.id.time_updated);
+        mapView         = (MapView)  findViewById(R.id.bmapView);
 
 
         location = Location.getInstance();
         location.addWatcher(this);
         updateView();
 
-//        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-//        LocationListener locationListener = new LocationListener() {
-//            @Override
-//            public void onLocationChanged(Location location) {
-//
-//                Log.i(TAG, "???" + location.getTime());
-//                Log.i(TAG, "???"+location.getLongitude());
-//                Log.i(TAG, "???"+location.getLatitude());
-//                Log.i(TAG, "???"+location.getAltitude());
-//
-//                Toast.makeText(getApplicationContext(),"locationChanged", Toast.LENGTH_SHORT).show();
-//                double latitude = location.getLatitude();
-//                double longitude = location.getLongitude();
-//
-//                latitudeText.setText("latitude: " + String.valueOf(latitude));
-//                longitudeText.setText("longitude: " + String.valueOf(longitude));
-//            }
-//
-//            @Override
-//            public void onStatusChanged(String provider, int status, Bundle extras) {
-//                switch (status) {
-//                    //GPS??????
-//                    case LocationProvider.AVAILABLE:
-//                        Log.i(TAG, "??GPS???????");
-//                        break;
-//                    //GPS????????
-//                    case LocationProvider.OUT_OF_SERVICE:
-//                        Log.i(TAG, "??GPS?????????");
-//                        break;
-//                    //GPS????????
-//                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
-//                        Log.i(TAG, "??GPS?????????");
-//                        break;
-//                }
-//                Toast.makeText(getApplicationContext(),"gps status changed", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onProviderEnabled(String provider) {
-//                Toast.makeText(getApplicationContext(),"gps opened", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            @Override
-//            public void onProviderDisabled(String provider) {
-//                Toast.makeText(getApplicationContext(),"gps Closed", Toast.LENGTH_SHORT).show();
-//            }
-//        };
-//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,locationListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 
     @Override
@@ -108,10 +101,37 @@ public class MainActivity extends Activity implements LocationWatcher{
         latitudeText.setText("latitude: " + String.valueOf(location.getLatitude()));
         longitudeText.setText("longitude: " + String.valueOf(location.getLongitude()));
         updateTime.setText("Last Update: " + location.getDate());
+
+        updateMapView();
+
+    }
+
+    private void updateMapView() {
+        BaiduMap map = mapView.getMap();
+        LatLng point = new LatLng(location.getLatitude(),location.getLongitude());
+
+        Bitmap bitmapOrg = BitmapFactory.decodeResource(getResources(), R.drawable.star);
+        Matrix matrix = new Matrix();
+        matrix.postScale((float)20.0/bitmapOrg.getWidth(),(float)20.0/bitmapOrg.getHeight());
+        Bitmap resizedBitmap = Bitmap.createBitmap(bitmapOrg, 0, 0,
+                bitmapOrg.getWidth(), bitmapOrg.getHeight(), matrix, true);
+
+        //BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromResource(R.drawable.star);
+
+        BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(resizedBitmap);
+
+        OverlayOptions option = new MarkerOptions().position(point).icon(bitmapDescriptor);
+        map.addOverlay(option);
+
+        MapStatus mapStatus = new MapStatus.Builder().target(point).zoom(12).build();
+        MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mapStatus);
+        map.setMapStatus(mapStatusUpdate);
     }
 
     @Override
     public void update() {
         updateView();
     }
+
+
 }
